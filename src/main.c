af4331c83768c32dfd125b3474817e969bac3995
WinterCore <hogobbl@gmail.com>
2024-08-10 22:43:47 +0400

    WIP

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "dbus/dbus-protocol.h"
 #include <stdio.h>
 #include <dbus/dbus.h>
 #include <stdlib.h>
@@ -7,7 +8,7 @@ int main() {
     DBusError error;
 
     dbus_error_init(&error);
-    DBusConnection *connection = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
+    DBusConnection *connection = dbus_bus_get(DBUS_BUS_SESSION, &error);
 
     if (dbus_error_is_set(&error)) {
         fprintf(stderr, "DBus Connection error: %s", error.message);
@@ -17,10 +18,43 @@ int main() {
     if (connection == NULL) {
         exit(EXIT_FAILURE);
     }
-    
 
     printf("Connected to D-Bus system bus!\n");
     
+    DBusMessage *message = dbus_message_new_method_call(
+        "org.freedesktop.Notifications",
+        "/org/freedesktop/Notifications",
+        "org.freedesktop.Notifications",
+        "GetCapabilities"
+    );
+
+    DBusMessage *reply = dbus_connection_send_with_reply_and_block(connection, message, -1, &error);
+
+    if (dbus_error_is_set(&error)) {
+        fprintf(stderr, "Failed to send message %s", error.message);
+        exit(EXIT_FAILURE);
+    }
+
+    dbus_message_unref(message);
+
+    if (reply) {
+        int msg_type = dbus_message_get_type(reply);
+        
+        DBusMessageIter iter;
+        dbus_message_iter_init(reply, &iter);
+
+        // dbus_message_iter_recurse();
+        // DBusMessageIter sub;
+
+        while (dbus_message_iter_has_next(&iter)) {
+            char *message;
+            dbus_message_iter_get_basic(&iter, &message);
+            dbus_message_iter_next(&iter);
+            printf("%s\n", message);
+        }
+    }
+
+ 
 
     dbus_connection_unref(connection);
 
